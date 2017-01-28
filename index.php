384a7b77e58e1712563866933e3fe52104de1c6e
Fernando Parreira <fparreira@gmail.com>
2017-01-28 18:18:33 -0200

    add projects list

--- index.php
+++ index.php
@@ -19,13 +19,13 @@
             <span class="texthelp"></span>
             <div class="secondaryText">
                 <a href="https://github.com/fparreira" role="link" target="_blank"><i
-                        class="fa fa-github fa-lg"></i></a>
+                            class="fa fa-github fa-lg"></i></a>
                 <a href="https://twitter.com/fjparreira" role="link" target="_blank"><i class="fa fa-twitter fa-lg"></i></a>
                 <a href="mailto:fparreira@gmail.com" role="link" target="_blank"><i
-                        class="fa fa-envelope fa-lg"></i></a>
+                            class="fa fa-envelope fa-lg"></i></a>
                 <a href="https://www.instagram.com/fjparreira" target="_blank"><i class="fa fa-instagram fa-lg"></i></a>
                 <a href="https://www.facebook.com/fernando.parreira" role="link" target="_blank"><i
-                        class="fa fa-facebook fa-lg"></i></a>
+                            class="fa fa-facebook fa-lg"></i></a>
             </div>
         </div>
     </div>
@@ -33,7 +33,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 article">
-            <h3>Skills</h3>
+            <h2>Skills</h2>
             <p>
                 I am a web developer for over 10 years. I live in São Paulo, Brazil. I've worked on projects involving
                 infrastructure, backend and frontend. I am always researching what's current in the field of web
@@ -44,10 +44,61 @@
     </div>
     <div class="row">
         <div class="col-md-12 article">
-            <h3>Skillshet</h3>
+            <h2>Skillset</h2>
             <p>
                 PHP, Laravel, JavaScript, jQuery, Bootstrap, WordPress (admin/themes), Nginx, Apache, Docker
             </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 article">
+            <h2>Some Projects</h2>
+            <?php
+
+            $repos = array(
+                array(
+                    'name' => 'apimovie',
+                    'url' => 'https://github.com/fparreira/apimovie',
+                    'description' => 'Simple Laravel API Movie DB'
+                ),
+                array(
+                    'name' => 'fparreira.com',
+                    'url' => 'https://github.com/fparreira/fparreira.com',
+                    'description' => 'Personal Website'
+                ),
+                array(
+                    'name' => 'blob-laravel',
+                    'url' => 'https://github.com/fparreira/blob-laravel',
+                    'description' => 'Image blob example in Laravel (create and read)'
+                ),
+                array(
+                    'name' => 'jquery-randomBG',
+                    'url' => 'https://github.com/fparreira/jquery-randomBG',
+                    'description' => 'Jquery plugin - Random Background Color'
+                ),
+                array(
+                    'name' => 'ifspvtp-WP',
+                    'url' => 'https://github.com/fparreira/ifspvtp-wp',
+                    'description' => 'WordPress theme for Federal Institute São Paulo'
+                ),
+
+            );
+
+            $len = count($repos);
+
+            for ($i = 0; $i < $len; $i++) {
+
+                ?>
+
+                <h3><a href="<?php echo $repos[$i]['url']; ?>" target="_blank"><?php echo $repos[$i]['name']; ?></a></h3>
+                <p><?php echo $repos[$i]['description']; ?></p>
+
+                <?php
+
+            }
+
+            ?>
+
         </div>
     </div>
 </div>
